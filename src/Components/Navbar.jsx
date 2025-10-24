8182b26e228f0ab69d18574aba56c42f12926c1f
Shajidaa <shajidaakterlopa101@gmail.com>
2025-10-25 01:47:02 +0600

    navbar for mobile version

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,85 +28,68 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className={`bg-[#fff8f7]! shadow-sm sticky top-0 z-50`}>
-      <MyContainer className="navbar">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
+    <>
+      <div className={`bg-[#fff8f7]! shadow-sm sticky top-0 z-50`}>
+        <MyContainer className="navbar">
+          <div className="navbar-start">
+            <div className="lg:h-22 h-15">
+              <Link to={"/"}>
+                <img
+                  className="w-full h-full object-contain"
+                  src={
+                    "https://i.ibb.co.com/mgLPLgj/7b902309-4336-4cf9-8ec6-c3bc50c557a2-removebg-preview.png"
+                  }
+                  alt=""
+                />
+              </Link>
             </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm  dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              {links}
-            </ul>
           </div>
-          <div className="h-22">
-            <Link to={"/"}>
-              <img
-                className="w-full h-full object-contain"
-                src={
-                  "https://i.ibb.co.com/mgLPLgj/7b902309-4336-4cf9-8ec6-c3bc50c557a2-removebg-preview.png"
-                }
-                alt=""
-              />
-            </Link>
+          <div className="navbar-center hidden md:flex">
+            <ul className="menu menu-horizontal px-1">{links}</ul>
           </div>
-        </div>
-        <div className="navbar-center hidden md:flex">
+          <div className="navbar-end">
+            {user ? (
+              <div className="flex justify-center items-center gap-2">
+                {" "}
+                <div
+                  data-tip={user.displayName}
+                  className="md:size-12 size-8 tooltip tooltip-error tooltip-bottom border-2 border-[#dc7977] rounded-full "
+                >
+                  <img
+                    className="w-full bg-cover h-full rounded-full  "
+                    src={user.photoURL}
+                    alt="User Profile"
+                  />
+                </div>
+                <Link
+                  onClick={handleLogOut}
+                  to={"login"}
+                  className="btn bg-linear-to-r  from-[#e69d95] to-blue-500  hover:from-[#ec958b] hover:to-blue-800  text-white "
+                >
+                  Log Out
+                </Link>
+              </div>
+            ) : (
+              <div className="flex justify-center items-center gap-2">
+                <TbUser className="md:size-8 size-5" />
+                <Link
+                  to={"login"}
+                  className="btn bg-linear-to-r from-[#e69d95] to-blue-500  hover:from-[#ec958b] hover:to-blue-800  text-white "
+                >
+                  Login
+                </Link>
+              </div>
+            )}
+          </div>
+        </MyContainer>
+      </div>
+
+      <div className="shadow-sm bg-base-100/30 backdrop-blur-md md:hidden fixed bottom-0 left-0 w-full z-50">
+        <div className=" flex justify-center items-center">
           <ul className="menu menu-horizontal px-1">{links}</ul>
         </div>
-        <div className="navbar-end">
-          {user ? (
-            <div className="flex justify-center items-center gap-2">
-              {" "}
-              <div
-                data-tip={user.displayName}
-                className="md:size-12 size-8 tooltip tooltip-error tooltip-bottom border-2 border-[#dc7977] rounded-full "
-              >
-                <img
-                  className="w-full bg-cover h-full rounded-full  "
-                  src={user.photoURL}
-                  alt="User Profile"
-                />
-              </div>
-              <Link
-                onClick={handleLogOut}
-                to={"login"}
-                className="btn bg-linear-to-r from-[#e69d95] to-blue-500  hover:from-[#ec958b] hover:to-blue-800  text-white "
-              >
-                Log Out
-              </Link>
-            </div>
-          ) : (
-            <div className="flex justify-center items-center gap-2">
-              <TbUser className="md:size-8 size-5" />
-              <Link
-                to={"login"}
-                className="btn bg-linear-to-r from-[#e69d95] to-blue-500  hover:from-[#ec958b] hover:to-blue-800  text-white "
-              >
-                Login
-              </Link>
-            </div>
-          )}
-        </div>
-      </MyContainer>
-    </div>
+      </div>
+    </>
   );
 };
 
