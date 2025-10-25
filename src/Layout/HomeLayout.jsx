c7de0a9c2ac8a24579289454508efe97f8ebf681
Shajidaa <shajidaakterlopa101@gmail.com>
2025-10-25 16:51:46 +0600

    loading off

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -2,24 +2,24 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
 import "@smastrom/react-rating/style.css";
-import { Outlet, useLocation } from "react-router";
-import { useContext, useEffect, useState } from "react";
+import { Outlet } from "react-router";
+import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthContext";
 import Spinner from "../Components/Spinner/Spinner";
 
 const HomeLayout = () => {
   const { loading } = useContext(AuthContext);
 
-  const location = useLocation();
-  const [pathLoading, setPathLoading] = useState(false);
+  // const location = useLocation();
+  // const [pathLoading, setPathLoading] = useState(false);
 
-  useEffect(() => {
-    setPathLoading(true);
-    const timer = setTimeout(() => setPathLoading(false), 700);
-    return () => clearTimeout(timer);
-  }, [location]);
+  // useEffect(() => {
+  //   setPathLoading(true);
+  //   const timer = setTimeout(() => setPathLoading(false), 700);
+  //   return () => clearTimeout(timer);
+  // }, [location]);
 
-  if (loading || pathLoading) {
+  if (loading) {
     return <Spinner></Spinner>;
   }
 
