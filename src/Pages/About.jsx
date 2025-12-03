f2cbed138bc2c8766a41280eb224a6d348da4ade
Shajidaa <shajidaakterlopa101@gmail.com>
2025-12-03 21:40:39 +0600

    u

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -9,7 +9,7 @@ import MyContainer from "../MyContainer/MyContainer";
 
 export default function About() {
   return (
-    <MyContainer className="min-h-screen  p-10 text-[#dc7977]">
+    <MyContainer className="min-h-screen  p-10 ">
       {/* Header Section */}
       <div className="text-center mb-5 md:mb-12">
         <h2 className="text-3xl text-[#333333] text-center font-bold mb-2 md:mb-4">
@@ -20,14 +20,12 @@ export default function About() {
       {/* About Section */}
       <div className="bg-white backdrop-blur-md p-10 rounded-3xl shadow-xl  mx-auto">
         <p className="text-lg leading-relaxed">
-          At <span className="font-semibold">Toytopia</span>, we believe every
-          child deserves a world filled with joy, creativity, and endless
-          adventure. Our collection is curated to bring sparkles of happiness
-          and learning to kids of all ages. Where imagination meets fun!
-          Discover the magical world crafted specially for kids.
-        </p>
-        <p className="mt-3 text-lg leading-relaxed">
-          From educational toys to pure fun, we focus on quality, safety, and
+          At <span className="font-semibold text-[#dc7977]">Toytopia</span>, we
+          believe every child deserves a world filled with joy, creativity, and
+          endless adventure. Our collection is curated to bring sparkles of
+          happiness and learning to kids of all ages. Where imagination meets
+          fun! Discover the magical world crafted specially for kids. From
+          educational toys to pure fun, we focus on quality, safety, and
           delightful experiences.
         </p>
       </div>
@@ -56,15 +54,17 @@ export default function About() {
             className="bg-white backdrop-blur-lg p-8 rounded-3xl text-center shadow-xl hover:scale-105 transition-transform"
           >
             <div className="flex justify-center mb-4">{item.icon}</div>
-            <h3 className="text-2xl font-bold mb-2">{item.title}</h3>
-            <p className="opacity-90">{item.desc}</p>
+            <h3 className="text-2xl font-bold text-[#dc7977] mb-2">
+              {item.title}
+            </h3>
+            <p className="opacity-90 text-[#dc7977]">{item.desc}</p>
           </div>
         ))}
       </div>
       {/* Mission Section */}
       <div className="mt-16 bg-white backdrop-blur-md p-10 rounded-3xl shadow-xl mx-auto">
         <h2 className="text-2xl font-bold  mb-4 text-center">Our Mission</h2>
-        <p className="text-lg leading-relaxed text-center">
+        <p className="text-lg leading-relaxed text-[#dc7977] text-center">
           Our mission is to build a joyful and imaginative world for kids by
           providing safe, high‑quality, and engaging toys that support learning
           and emotional growth.
--- src/Provider/CardProvider.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CardProvider = () => {
-  return <div></div>;
-};
-
-export default CardProvider;
